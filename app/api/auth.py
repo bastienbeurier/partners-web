a63d9d15c1cb79d7d08e4f10e98eaa38dd78b67b
Bastien Beurier <bastienbeurier@gmail.com>
2018-12-17 12:06:14 +0100

    Change basic auth

--- app/api/auth.py
+++ app/api/auth.py
@@ -9,10 +9,8 @@ token_auth = HTTPTokenAuth()
 
 
 @basic_auth.verify_password
-def verify_password(email, password):
-    print("email" + email)
-    print("password" + password)
-    user = User.query.filter_by(email=email).first()
+def verify_password(username, password):
+    user = User.query.filter_by(email=username).first()
     if user is None:
         return False
     g.current_user = user
